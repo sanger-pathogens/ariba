b9bb4d4beb48161e70ed1264dd6accd26fa3cf7d
Martin Hunt <mh12@sanger.ac.uk>
2015-02-17 14:03:29 +0000

    Be verbose when required

--- ariba/mapping.py
+++ ariba/mapping.py
@@ -15,7 +15,8 @@ def run_smalt(
       max_insert=1000,
       minid=0.9,
       sort=False,
-      extra_smalt_map_ops='-x'
+      extra_smalt_map_ops='-x',
+      verbose=False
     ):
     if extra_smalt_map_ops is None:
         extra_smalt_map_ops = ''
@@ -59,15 +60,15 @@ def run_smalt(
         intermediate_bam = final_bam
 
     map_cmd += ' -bS -T ' + ref_fa + '  - > ' + intermediate_bam
-    common.syscall(index_cmd)
-    common.syscall(map_cmd)
+    common.syscall(index_cmd, verbose=verbose)
+    common.syscall(map_cmd, verbose=verbose)
 
     if sort:
         threads = min(4, threads)
         thread_mem = int(500 / threads)
         sort_cmd = 'samtools sort -@' + str(threads) + ' -m ' + str(thread_mem) + 'M ' + intermediate_bam + ' ' + out_prefix
         index_cmd = 'samtools index ' + final_bam
-        common.syscall(sort_cmd)
-        common.syscall(index_cmd)
+        common.syscall(sort_cmd, verbose=verbose)
+        common.syscall(index_cmd, verbose=verbose)
     for fname in clean_files:
         os.unlink(fname)
