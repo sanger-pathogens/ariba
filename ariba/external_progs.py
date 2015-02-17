43519da547ae356487eeb66f765f4778e541cc60
Martin Hunt <mh12@sanger.ac.uk>
2015-02-17 16:00:26 +0000

    Fix bugs finding paths of dependencies

--- ariba/external_progs.py
+++ ariba/external_progs.py
@@ -14,15 +14,15 @@ def is_in_path(prog):
 
 
 prog_to_version_cmd = {
-    'bcftools': ('bcftools', re.compile('^Version: ([0-9\.]+)')),
-    'nucmer': ('nucmer --version', re.compile('^NUCmer \(NUCleotide MUMmer\) version ([0-9\.]+)')),
-    'smalt': ('smalt version', re.compile('^Version: ([0-9\.]+)')),
-    'spades': ('spades.py', re.compile('^SPAdes genome assembler v.([0-9\.]+)')),
-    'samtools': ('samtools', re.compile('^Version: ([0-9\.]+)')),
-    'sspace': (None, re.compile('^Usage: .*pl \[SSPACE_(.*)\]')),
-    'gapfiller': (None, re.compile('^Usage: .*pl \[GapFiller_(.*)\]')),
-    'velvetg': ('velvetg', re.compile('Version ([0-9\.]+)')),
-    'velveth': ('velveth', re.compile('Version ([0-9\.]+)')),
+    'bcftools': ('', re.compile('^Version: ([0-9\.]+)')),
+    'nucmer': ('--version', re.compile('^NUCmer \(NUCleotide MUMmer\) version ([0-9\.]+)')),
+    'smalt': ('version', re.compile('^Version: ([0-9\.]+)')),
+    'spades': ('', re.compile('^SPAdes genome assembler v.([0-9\.]+)')),
+    'samtools': ('', re.compile('^Version: ([0-9\.]+)')),
+    'sspace': ('', re.compile('^Usage: .*pl \[SSPACE_(.*)\]')),
+    'gapfiller': ('', re.compile('^Usage: .*pl \[GapFiller_(.*)\]')),
+    'velvetg': ('', re.compile('Version ([0-9\.]+)')),
+    'velveth': ('', re.compile('Version ([0-9\.]+)')),
 }
 
 
@@ -30,33 +30,36 @@ def get_version(prog, path=None):
     assert prog in prog_to_version_cmd
     if path is None:
         path = prog
+
     if not is_in_path(path):
         raise Error('Error getting version of ' + path + ' - not found in path.')
+
+    path = shutil.which(path)
 
     if prog in ['sspace', 'gapfiller']:
         cmd = 'perl ' + os.path.realpath(shutil.which(path))
         regex = prog_to_version_cmd[prog][1]
     else:
         cmd, regex = prog_to_version_cmd[prog]
+        cmd = path + ' ' + cmd
 
     cmd_output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     cmd_output = common.decode(cmd_output[0]).split('\n')[:-1] + common.decode(cmd_output[1]).split('\n')[:-1]
     for line in cmd_output:
         hits = regex.search(line)
         if hits:
-            return hits.group(1)
-    return 'UNKNOWN ...\n I tried running this to get the version: "' + cmd + '"\n and the output didn\'t match this regular expression: "' + regex.pattern + '"'
-
+            return hits.group(1), path
+    return 'UNKNOWN ...\n I tried running this to get the version: "' + cmd + '"\n and the output didn\'t match this regular expression: "' + regex.pattern + '"', path
 
 
 def check_versions(opts, verbose=False):
     if verbose:
         print('{:_^79}'.format(' Checking dependencies and their versions '))
-        print('tool', 'path', 'version', sep='\t')
+        print('tool', 'version', 'path', sep='\t')
 
     l = [
         ('bcftools', opts.bcftools),
-        ('nucmer', None),
+        ('nucmer', 'nucmer'),
         ('smalt', opts.smalt),
         ('samtools', opts.samtools),
         ('sspace', opts.sspace),
@@ -84,16 +87,18 @@ def check_versions(opts, verbose=False):
     errors = []
 
     for t in l:
-        version = get_version(t[0], path=t[1])
+        version, path = get_version(t[0], path=t[1])
         if verbose:
-            print(t[0], t[1], version, sep='\t')
+            print(t[0], version, path, sep='\t')
 
         if t[0] in min_versions and LooseVersion(version) < LooseVersion(min_versions[t[0]]):
-            errors.append(' '.join(['Found version', version, 'of', t[0], 'which is too low! Please update to at least', min_versions[t[0]]]))
+            errors.append(' '.join(['Found version', version, 'of', t[0], 'which is too low! Please update to at least', min_versions[t[0]] + '\n   Found it here:', path]))
 
     if len(errors):
         for e in errors:
+            print('\n*** Error! Bad dependency! ***', file=sys.stderr)
             print(e, file=sys.stderr)
+            print()
         raise Error('Cannot continue. Some dependencies need updating')
 
     if verbose:
