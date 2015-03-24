faf127bae9ed5e3cddc88120f12c32f1d149a237
Martin Hunt <mh12@sanger.ac.uk>
2015-03-24 09:21:14 +0000

    Set default genetic code to 11

--- ariba/tasks/refcheck.py
+++ ariba/tasks/refcheck.py
@@ -7,7 +7,7 @@ def run():
     parser = argparse.ArgumentParser(
         description = 'Check or fix resistance genes FASTA file',
         usage = 'ariba refcheck [options] <infile>')
-    parser.add_argument('--genetic_code', type=int, help='Number of genetic code to use. Currently supported 1 (default) 4, 11', choices=[1,4,11], default=1, metavar='INT')
+    parser.add_argument('--genetic_code', type=int, help='Number of genetic code to use. Currently supported 1,4,11 [%(default)s]', choices=[1,4,11], default=11, metavar='INT')
     parser.add_argument('-m', '--min_length', type=int, help='Minimum length in nucleotides of gene [%(default)s]', metavar='INT', default=6)
     parser.add_argument('-o', '--outprefix', help='Prefix of output files. If this option is used, a fixed file will be output, together with information on what was changed in the input file. If this option is not used, the script dies if any input sequence is not OK')
     parser.add_argument('infile', help='Input file containing genes to be checked', metavar='Filename')
