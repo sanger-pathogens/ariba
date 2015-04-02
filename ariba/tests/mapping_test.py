0daf7034e456c316355672b407c7c4a07191fa10
Martin Hunt <mh12@sanger.ac.uk>
2015-04-02 15:51:33 +0100

    Add run_bowtie2()

--- ariba/tests/mapping_test.py
+++ ariba/tests/mapping_test.py
@@ -33,14 +33,43 @@ class TestMapping(unittest.TestCase):
         assert(shutil.which('samtools') is not None)
 
 
+    def test_run_bowtie2(self):
+        '''Test run_bowtie2 unsorted'''
+        self.maxDiff = None
+        ref = os.path.join(data_dir, 'mapping_test_bowtie2_ref.fa')
+        reads1 = os.path.join(data_dir, 'mapping_test_bowtie2_reads_1.fq')
+        reads2 = os.path.join(data_dir, 'mapping_test_bowtie2_reads_2.fq')
+        out_prefix = 'tmp.out.bowtie2'
+        mapping.run_bowtie2(reads1, reads2, ref, out_prefix)
+        expected = get_sam_columns(os.path.join(data_dir, 'mapping_test_bowtie2_unsorted.bam'))
+        got = get_sam_columns(out_prefix + '.bam')
+        self.assertListEqual(expected, got)
+        os.unlink(out_prefix + '.bam')
+
+
+    def test_run_bowtie2_and_sort(self):
+        '''Test run_bowtie2 sorted'''
+        ref = os.path.join(data_dir, 'mapping_test_bowtie2_ref.fa')
+        reads1 = os.path.join(data_dir, 'mapping_test_bowtie2_reads_1.fq')
+        reads2 = os.path.join(data_dir, 'mapping_test_bowtie2_reads_2.fq')
+        out_prefix = 'tmp.out.bowtie2'
+        mapping.run_bowtie2(reads1, reads2, ref, out_prefix, sort=True)
+        expected = get_sam_columns(os.path.join(data_dir, 'mapping_test_bowtie2_sorted.bam'))
+        got = get_sam_columns(out_prefix + '.bam')
+        self.assertListEqual(expected, got)
+        os.unlink(out_prefix + '.bam')
+        os.unlink(out_prefix + '.bam.bai')
+        os.unlink(out_prefix + '.unsorted.bam')
+
+
     def test_run_smalt(self):
         '''Test run_smalt unsorted'''
-        ref = os.path.join(data_dir, 'mapping_test_ref.fa')
-        reads1 = os.path.join(data_dir, 'mapping_test_reads_1.fq')
-        reads2 = os.path.join(data_dir, 'mapping_test_reads_2.fq')
-        out_prefix = 'tmp.out'
+        ref = os.path.join(data_dir, 'mapping_test_smalt_ref.fa')
+        reads1 = os.path.join(data_dir, 'mapping_test_smalt_reads_1.fq')
+        reads2 = os.path.join(data_dir, 'mapping_test_smalt_reads_2.fq')
+        out_prefix = 'tmp.out.smalt'
         mapping.run_smalt(reads1, reads2, ref, out_prefix)
-        expected = get_sam_columns(os.path.join(data_dir, 'mapping_test_unsorted.bam'))
+        expected = get_sam_columns(os.path.join(data_dir, 'mapping_test_smalt_unsorted.bam'))
         got = get_sam_columns(out_prefix + '.bam')
         self.assertListEqual(expected, got)
         os.unlink(out_prefix + '.bam')
@@ -48,12 +77,12 @@ class TestMapping(unittest.TestCase):
 
     def test_run_smalt_and_sort(self):
         '''Test run_smalt sorted'''
-        ref = os.path.join(data_dir, 'mapping_test_ref.fa')
-        reads1 = os.path.join(data_dir, 'mapping_test_reads_1.fq')
-        reads2 = os.path.join(data_dir, 'mapping_test_reads_2.fq')
-        out_prefix = 'tmp.out'
+        ref = os.path.join(data_dir, 'mapping_test_smalt_ref.fa')
+        reads1 = os.path.join(data_dir, 'mapping_test_smalt_reads_1.fq')
+        reads2 = os.path.join(data_dir, 'mapping_test_smalt_reads_2.fq')
+        out_prefix = 'tmp.out.smalt'
         mapping.run_smalt(reads1, reads2, ref, out_prefix, sort=True)
-        expected = get_sam_columns(os.path.join(data_dir, 'mapping_test_sorted.bam'))
+        expected = get_sam_columns(os.path.join(data_dir, 'mapping_test_smalt_sorted.bam'))
         got = get_sam_columns(out_prefix + '.bam')
         self.assertListEqual(expected, got)
         os.unlink(out_prefix + '.bam')
