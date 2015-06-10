5faa9ff5fbddfda8b8b3aa673a7e3b045d210109
Martin Hunt <mh12@sanger.ac.uk>
2015-06-10 16:11:38 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ariba',
-    version='0.3.2',
+    version='0.4.0',
     description='ARIBA: Antibiotic Resistance Identification By Assembly',
     packages = find_packages(),
     author='Martin Hunt',
