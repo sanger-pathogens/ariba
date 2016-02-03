442d0e76c1e5a1e28719a70dddcb8890f880b1c5
Martin Hunt <mh12@sanger.ac.uk>
2016-02-03 13:54:09 +0000

    Simplify by not renaming, use -d option to cdhit instead

--- ariba/cdhit.py
+++ ariba/cdhit.py
@@ -6,8 +6,6 @@ from ariba import common
 
 class Error (Exception): pass
 
-
-
 class Runner:
     def __init__(
       self,
@@ -32,110 +30,83 @@ class Runner:
 
     def fake_run(self):
         '''Doesn't actually run cd-hit. Instead, puts each input sequence into its own cluster. So it's as if cdhit was run, but didn't cluster anything'''
-        cluster_to_name = {}
-        found_names = set()
+        clusters = {}
         seq_reader = pyfastaq.sequences.file_reader(self.infile)
         f = pyfastaq.utils.open_file_write(self.outfile)
+
         for seq in seq_reader:
-            if seq.id in found_names:
+            if seq.id in clusters:
                 pyfastaq.utils.close(f)
                 raise Error('Sequence name "' + seq.id + '" not unique. Cannot continue')
-            found_names.add(seq.id)
-            cluster_number = str(len(cluster_to_name))
-            cluster_to_name[cluster_number] = {seq.id}
-            seq.id = cluster_number
+
+            clusters[seq.id] = {seq.id}
             print(seq, file=f)
 
         pyfastaq.utils.close(f)
-        return cluster_to_name
+        return clusters
+
+
+    def _get_ids(self, infile):
+        seq_reader = pyfastaq.sequences.file_reader(infile)
+        return set([seq.id for seq in seq_reader])
+
+
+    @staticmethod
+    def _parse_cluster_info_file(infile, cluster_representatives):
+        f = pyfastaq.utils.open_file_read(infile)
+        clusters = {}
+        current_cluster = None
+
+        for line in f:
+            data = line.rstrip().split()
+            seqname = data[2]
+            if not (seqname.startswith('>') and seqname.endswith('...')):
+                raise Error('Unexpected format of line from cdhit output file "' + infile + '". Line is:\n' + line)
+            seqname = seqname[1:-3]
+
+            if data[3] == '*':
+                current_cluster = seqname
+                assert current_cluster not in clusters
+                clusters[current_cluster] = {current_cluster}
+            else:
+                assert current_cluster in clusters
+                if seqname in clusters[current_cluster]:
+                    raise Error('Duplicate name "' + seqname + '" found in cluster ' + cluster)
+
+                clusters[current_cluster].add(seqname)
+
+        pyfastaq.utils.close(f)
+        if set(clusters.keys()) != cluster_representatives:
+            raise Error('Mismatch in cdhit output sequence names between fasta file and clusters file. Cannot continue')
+
+        return clusters
 
 
     def run(self):
         tmpdir = tempfile.mkdtemp(prefix='tmp.run_cd-hit.', dir=os.getcwd())
         cdhit_fasta = os.path.join(tmpdir, 'cdhit')
         cluster_info_outfile = cdhit_fasta + '.bak.clstr'
-        infile_renamed = os.path.join(tmpdir, 'input.renamed.fa')
-
-        # cd-hit truncates all names to 19 bases in its report of which
-        # sequences belong to which clusters. So need to temporarily
-        # rename all sequences to have short enough names. Grrr.
-        new_to_old_name = self._enumerate_fasta(self.infile, infile_renamed)
 
         cmd = ' '.join([
             'cd-hit-est',
-            '-i', infile_renamed,
+            '-i', self.infile,
             '-o', cdhit_fasta,
             '-c', str(self.seq_identity_threshold),
             '-T', str(self.threads),
             '-s', str(self.length_diff_cutoff),
+            '-d 0',
             '-bak 1',
         ])
 
         common.syscall(cmd, verbose=self.verbose)
-
         cluster_representatives = self._get_ids(cdhit_fasta)
-        clusters, cluster_rep_to_cluster = self._parse_cluster_info_file(cluster_info_outfile, new_to_old_name, cluster_representatives)
-        self._rename_fasta(cdhit_fasta, self.outfile, cluster_rep_to_cluster)
+        clusters = self._parse_cluster_info_file(cluster_info_outfile, cluster_representatives)
+
+        try:
+            os.rename(cdhit_fasta, self.outfile)
+        except:
+            raise Error('Error rname ' + cdhit_fasta + ' ' + self.outfile + '. Cannot continue')
+
         shutil.rmtree(tmpdir)
         return clusters
-
-
-    def _enumerate_fasta(self, infile, outfile):
-        rename_file = outfile + '.tmp.rename_info'
-        assert not os.path.exists(rename_file)
-        pyfastaq.tasks.enumerate_names(infile, outfile, rename_file=rename_file)
-
-        with open(rename_file) as f:
-            lines = [x.rstrip().split('\t') for x in f.readlines() if x != '#old\tnew\n']
-            new_to_old_name = {x[1]: x[0] for x in lines}
-            if len(lines) != len(new_to_old_name):
-                raise Error('Sequence names in input file not unique! Cannot continue')
-
-        os.unlink(rename_file)
-        return new_to_old_name
-
-
-    def _rename_fasta(self, infile, outfile, names_dict):
-        seq_reader = pyfastaq.sequences.file_reader(infile)
-        f = pyfastaq.utils.open_file_write(outfile)
-        for seq in seq_reader:
-            seq.id = names_dict[seq.id]
-            print(seq, file=f)
-
-        pyfastaq.utils.close(f)
-
-
-    def _parse_cluster_info_file(self, infile, names_dict, cluster_representatives):
-        f = pyfastaq.utils.open_file_read(infile)
-        clusters = {}
-        cluster_representative_to_cluster_number = {}
-        for line in f:
-            data = line.rstrip().split()
-            cluster = data[0]
-            seqname = data[2]
-            if not (seqname.startswith('>') and seqname.endswith('...')):
-                raise Error('Unexpected format of sequence name in line:\n' + line)
-            seqname = seqname[1:-3]
-
-            if seqname in cluster_representatives:
-                cluster_representative_to_cluster_number[seqname] = cluster
-
-            seqname = names_dict[seqname]
-
-            if cluster not in clusters:
-                clusters[cluster] = set()
-
-            if seqname in clusters[cluster]:
-                raise Error('Duplicate name "' + seqname + '" found in cluster ' + str(cluster))
-
-            clusters[cluster].add(seqname)
-
-        pyfastaq.utils.close(f)
-
-        return clusters, cluster_representative_to_cluster_number
-
-
-    def _get_ids(self, infile):
-        seq_reader = pyfastaq.sequences.file_reader(infile)
-        return set([seq.id for seq in seq_reader])
 
--- ariba/tests/cdhit_test.py
+++ ariba/tests/cdhit_test.py
@@ -13,19 +13,6 @@ class TestCdhit(unittest.TestCase):
             r = cdhit.Runner('oopsnotafile', 'out')
 
 
-    def test_enumerate_fasta(self):
-        '''test _enumerate_fasta'''
-        infile = os.path.join(data_dir, 'cdhit_test_enumerate_fasta.in.fa')
-        expected_outfile = os.path.join(data_dir, 'cdhit_test_enumerate_fasta.out.fa')
-        tmpfile = 'tmp.test_enumerate_fasta.out.fa'
-        expected_dict = {'1': 'a', '2': 'b', '3': 'c'}
-        r = cdhit.Runner(infile, 'out')
-        got_dict = r._enumerate_fasta(infile, tmpfile)
-        self.assertTrue(filecmp.cmp(expected_outfile, tmpfile, shallow=False))
-        self.assertEqual(expected_dict, got_dict)
-        os.unlink(tmpfile)
-
-
     def test_get_ids(self):
         '''test _get_ids'''
         infile = os.path.join(data_dir, 'cdhit_test_get_ids.fa')
@@ -37,31 +24,14 @@ class TestCdhit(unittest.TestCase):
 
     def test_parse_cluster_info_file(self):
         '''test _parse_cluster_info_file'''
-        infile = os.path.join(data_dir, 'cdhit_test_parse_cluster_info_file.in.fa')
-        r = cdhit.Runner(infile, 'out')
-        names_dict = {str(i): 'seq' + str(i) for i in range(1,5)}
-        cluster_representatives = {'1', '4'}
-        cluster_file = os.path.join(data_dir, 'cdhit_test_parse_cluster_info_file.out.fa.bak.clstr')
-        got_clusters, got_reps = r._parse_cluster_info_file(cluster_file, names_dict, cluster_representatives)
+        cluster_representatives = {'seq1', 'seq4'}
+        infile = os.path.join(data_dir, 'cdhit_test_parse_cluster_info_file.infile')
+        got_clusters = cdhit.Runner._parse_cluster_info_file(infile, cluster_representatives)
         expected_clusters = {
-            '0': {'seq1', 'seq2', 'seq3'},
-            '1': {'seq4'}
+            'seq1': {'seq1', 'seq2', 'seq3'},
+            'seq4': {'seq4'}
         }
-        expected_reps = {'1': '0', '4': '1'}
         self.assertEqual(expected_clusters, got_clusters)
-        self.assertEqual(expected_reps, got_reps)
-
-
-    def test_rename_fasta(self):
-        '''test _rename_fasta'''
-        infile = os.path.join(data_dir, 'cdhit_test_rename_fasta.in.fa')
-        tmpfile = 'tmp.rename_fasta.out.fa'
-        expected = os.path.join(data_dir, 'cdhit_test_rename_fasta.out.fa')
-        names_dict = {'a': 'seq1', 'b': 'seq2', 'c': 'seq3'}
-        r = cdhit.Runner(infile, 'out')
-        r._rename_fasta(infile, tmpfile, names_dict)
-        self.assertTrue(filecmp.cmp(expected, tmpfile, shallow=False))
-        os.unlink(tmpfile)
 
 
     def test_run(self):
@@ -72,8 +42,8 @@ class TestCdhit(unittest.TestCase):
         r = cdhit.Runner(infile, tmpfile)
         clusters = r.run()
         expected_clusters = {
-            '0': {'seq1', 'seq2', 'seq3'},
-            '1': {'seq4'},
+            'seq1': {'seq1', 'seq2', 'seq3'},
+            'seq4': {'seq4'},
         }
         self.assertEqual(clusters, expected_clusters)
         self.assertTrue(filecmp.cmp(tmpfile, expected_outfile, shallow=False))
@@ -88,10 +58,10 @@ class TestCdhit(unittest.TestCase):
         r = cdhit.Runner(infile, tmpfile)
         clusters = r.fake_run()
         expected_clusters = {
-            '0': {'seq1'},
-            '1': {'seq2'},
-            '2': {'seq3'},
-            '3': {'seq4'},
+            'seq1': {'seq1'},
+            'seq2': {'seq2'},
+            'seq3': {'seq3'},
+            'seq4': {'seq4'},
         }
         self.assertEqual(clusters, expected_clusters)
         self.assertTrue(filecmp.cmp(tmpfile, expected_outfile, shallow=False))
