b69eecfa1cc60da4bf4e04cb2891be970a9ced42
Martin Hunt <mh12@sanger.ac.uk>
2016-04-05 14:32:47 +0100

    add option --exclude_flags

--- ariba/tasks/reportfilter.py
+++ ariba/tasks/reportfilter.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 import ariba
 
 def run():
@@ -6,6 +7,7 @@ def run():
         description = 'Filters an ARIBA report tsv file',
         usage = 'ariba reportfilter [options] <infile> <outprefix>'
     )
+    parser.add_argument('--exclude_flags', help='Comma-separated list of flags to exclude. [%(default)s]', default='assembly_fail,ref_seq_choose_fail')
     parser.add_argument('--min_pc_id', type=float, help='Minimum percent identity of nucmer match between contig and reference [%(default)s]', default=90.0, metavar='FLOAT')
     parser.add_argument('--min_ref_base_asm', type=int, help='Minimum number of reference bases matching assembly [%(default)s]', default=1, metavar='INT')
     parser.add_argument('--keep_syn', action='store_true', help='Keep synonymous variants (by default they are removed')
@@ -13,6 +15,14 @@ def run():
     parser.add_argument('infile', help='Name of input tsv file')
     parser.add_argument('outprefix', help='Prefix of output files. outprefix.tsv and outprefix.xls will be made')
     options = parser.parse_args()
+
+    flags_to_exclude = options.exclude_flags.split(',')
+    allowed_flags = set(ariba.flag.flags_in_order)
+    bad_flags = [x for x in flags_to_exclude if x not in allowed_flags]
+    if len(bad_flags):
+        print('Error in option --exclude_flags. The following were not recognised:', ','.join(bad_flags), file=sys.stderr)
+        print('Must choose from:', ','.join(ariba.flag.flags_in_order), file=sys.stderr)
+        sys.exit(1)
 
     rf = ariba.report_filter.ReportFilter(
         infile=options.infile,
