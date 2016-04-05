acaa2e2446e06352a8db8374a83a836abafe44bd
Martin Hunt <mh12@sanger.ac.uk>
2016-04-05 14:26:36 +0100

    Add option --keep_syn

--- ariba/tasks/reportfilter.py
+++ ariba/tasks/reportfilter.py
@@ -8,6 +8,7 @@ def run():
     )
     parser.add_argument('--min_pc_id', type=float, help='Minimum percent identity of nucmer match between contig and reference [%(default)s]', default=90.0, metavar='FLOAT')
     parser.add_argument('--min_ref_base_asm', type=int, help='Minimum number of reference bases matching assembly [%(default)s]', default=1, metavar='INT')
+    parser.add_argument('--keep_syn', action='store_true', help='Keep synonymous variants (by default they are removed')
     parser.add_argument('--keep_without_known_var', action='store_true', help='Use this option to not filter out where there is a known variant, but the assembly has the wild type. By default these rows are removed.')
     parser.add_argument('infile', help='Name of input tsv file')
     parser.add_argument('outprefix', help='Prefix of output files. outprefix.tsv and outprefix.xls will be made')
@@ -18,6 +19,7 @@ def run():
         min_pc_ident=options.min_pc_id,
         min_ref_base_assembled=options.min_ref_base_asm,
         ignore_not_has_known_variant=not options.keep_without_known_var,
+        remove_synonymous_snps=not options.keep_syn,
     )
     rf.run(options.outprefix)
 
