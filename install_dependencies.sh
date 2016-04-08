c5d942231ab1698f4d8d8683adc1580cf8030bcc
Martin Hunt <mh12@sanger.ac.uk>
2016-04-08 17:05:03 +0100

    Add R ape package

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -105,3 +105,10 @@ update_path ${mummer_dir}
 update_path ${samtools_dir}
 update_path ${spades_dir}
 
+
+# -------------- R packages ---------------
+mkdir -p ~/R/libs
+echo "R_LIBS=~/R/libs" > ~/.Renviron
+wget https://cran.r-project.org/src/contrib/ape_3.4.tar.gz
+R CMD INSTALL ape_3.4.tar.gz
+
