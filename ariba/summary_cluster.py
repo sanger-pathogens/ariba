f3c80ad37548f5f396349defa350faa93595b1c4
martinghunt <martin.g.hunt@gmail.com>
2016-04-25 10:29:53 +0000

    Add SummaryCluster

---
+++ ariba/summary_cluster.py
@@ -0,0 +1,63 @@
+from ariba import flag, report
+
+class Error (Exception): pass
+
+int_columns = [
+    'reads',
+    'ref_len',
+    'ref_base_assembled',
+    'ctg_len',
+    'ref_start',
+    'ref_end',
+    'ctg_start',
+    'ctg_end',
+]
+
+
+float_columns = ['pc_ident']
+
+class SummaryCluster:
+    def __init__(self):
+        self.name = None
+        self.data = {}
+
+
+    @classmethod
+    def line2dict(cls, line):
+        data = line.rstrip().split('\t')
+        if len(data) != len(report.columns):
+            raise Error('Wrong number of columns in the following line. Expected ' + str(len(report.columns)) + ' but got ' + str(len(data)) + '\n' + line)
+        d = {report.columns[i]: data[i] for i in range(len(data))}
+        try:
+            d['flag'] = flag.Flag(int(d['flag']) )
+        except:
+            raise Error('Error getting flag in the following line. Got "' + d['flag'] + '" for the flag.\n' + line)
+
+        for key in int_columns:
+            try:
+                d[key] = int(d[key])
+            except:
+                assert d[key] == '.'
+
+        for key in float_columns:
+            try:
+                d[key] = float(d[key])
+            except:
+                assert d[key] == '.'
+
+        return d
+
+
+    def add_data_dict(self, data_dict):
+        if self.name is None:
+            self.name = data_dict['cluster']
+
+        if self.name != data_dict['cluster']:
+            raise Error('Cannot add dict to SummaryCluster. Expected cluster name "' + self.name + '" but got "' + data_dict['cluster'] + '".')
+
+        if data_dict['ref_name'] not in self.data:
+            self.data[data_dict['ref_name']] = []
+
+        self.data[data_dict['ref_name']].append(data_dict)
+
+
---
+++ ariba/tests/summary_cluster_test.py
@@ -0,0 +1,64 @@
+import unittest
+import copy
+import filecmp
+import os
+from ariba import flag, summary_cluster
+
+modules_dir = os.path.dirname(os.path.abspath(summary_cluster.__file__))
+data_dir = os.path.join(modules_dir, 'tests', 'data')
+
+class TestSummaryCluster(unittest.TestCase):
+    def test_line2dict(self):
+        '''Test _line2dict'''
+        line = 'refname\treftype\t19\t78\tcluster\t120\t120\t98.33\tctg_name\t279\t24.4\t1\tSNP\tn\tA14T\t1\tA14T\tSNP\t13\t13\tA\t84\t84\tT\t17\t.\t17\tnoncoding1_n_A14T_N_ref has wild type, foo bar\tsome free text'
+
+        expected = {
+            'ref_name': 'refname',
+            'ref_type': 'reftype',
+            'flag': flag.Flag(19),
+            'reads': 78,
+            'cluster': 'cluster',
+            'ref_len': 120,
+            'ref_base_assembled': 120,
+            'pc_ident': 98.33,
+            'ctg': 'ctg_name',
+            'ctg_len': 279,
+            'ctg_cov': '24.4',
+            'known_var': '1',
+            'var_type': 'SNP',
+            'var_seq_type': 'n',
+            'known_var_change': 'A14T',
+            'has_known_var': '1',
+            'ref_ctg_change': 'A14T',
+            'ref_ctg_effect': 'SNP',
+            'ref_start': 13,
+            'ref_end': 13,
+            'ref_nt': 'A',
+            'ctg_start': 84,
+            'ctg_end': 84,
+            'ctg_nt': 'T',
+            'smtls_total_depth': '17',
+            'smtls_alt_nt': '.',
+            'smtls_alt_depth': '17',
+            'var_description': 'noncoding1_n_A14T_N_ref has wild type, foo bar',
+            'free_text': 'some free text'
+        }
+
+        self.assertEqual(summary_cluster.SummaryCluster.line2dict(line), expected)
+
+
+    def test_add_data_dict(self):
+        '''Test add_data_dict'''
+        cluster = summary_cluster.SummaryCluster()
+        self.assertTrue(cluster.name is None)
+        line1 = 'refname\treftype\t19\t78\tcluster\t120\t120\t98.33\tctg_name\t279\t24.4\t1\tSNP\tn\tA14T\t1\tA14T\tSNP\t13\t13\tA\t84\t84\tT\t17\t.\t17\tnoncoding1_n_A14T_N_ref has wild type, foo bar\tsome free text'
+        line2 = 'refname\treftype\t19\t78\tcluster2\t120\t120\t98.33\tctg_name\t279\t24.4\t1\tSNP\tn\tA14T\t1\tA14T\tSNP\t13\t13\tA\t84\t84\tT\t17\t.\t17\tnoncoding1_n_A14T_N_ref has wild type, foo bar\tsome free text'
+        data_dict1 = summary_cluster.SummaryCluster.line2dict(line1)
+        data_dict2 = summary_cluster.SummaryCluster.line2dict(line2)
+        cluster.add_data_dict(data_dict1)
+        self.assertEqual(cluster.name, data_dict1['cluster'])
+        self.assertEqual(cluster.data, {data_dict1['ref_name']: [data_dict1]})
+        with self.assertRaises(summary_cluster.Error):
+            cluster.add_data_dict(data_dict2)
+
+
