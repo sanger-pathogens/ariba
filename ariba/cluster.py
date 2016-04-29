39a20f538e8283146a11468cec30b7f435838ebe
martinghunt <martin.g.hunt@gmail.com>
2016-04-29 13:12:53 +0000

    Make input files if root_dir does not exist

--- ariba/cluster.py
+++ ariba/cluster.py
@@ -17,6 +17,8 @@ class Cluster:
       refdata,
       total_reads,
       total_reads_bases,
+      read_store=None,
+      reference_names=None,
       logfile=None,
       assembly_coverage=50,
       assembly_kmer=21,
@@ -42,13 +44,14 @@ class Cluster:
       extern_progs=None,
       random_seed=42,
     ):
-
         self.root_dir = os.path.abspath(root_dir)
-        if not os.path.exists(self.root_dir):
-            raise Error('Directory ' + self.root_dir + ' not found. Cannot continue')
-
-        self.name = name
+        self.read_store = read_store
         self.refdata = refdata
+        self.name = name
+        self.reference_fa = os.path.join(self.root_dir, 'reference.fa')
+        self.reference_names = reference_names
+        self._set_up_input_files()
+
         self.total_reads = total_reads
         self.total_reads_bases = total_reads_bases
         self.logfile = logfile
@@ -60,12 +63,8 @@ class Cluster:
         self.reads_insert = reads_insert
         self.spades_other_options = spades_other_options
 
-        self.all_reads1 = os.path.join(self.root_dir, 'reads_1.fq')
-        self.all_reads2 = os.path.join(self.root_dir, 'reads_2.fq')
         self.reads_for_assembly1 = os.path.join(self.root_dir, 'reads_for_assembly_1.fq')
         self.reads_for_assembly2 = os.path.join(self.root_dir, 'reads_for_assembly_2.fq')
-        self.reference_fa = os.path.join(self.root_dir, 'reference.fa')
-        self.references_fa = os.path.join(self.root_dir, 'references.fa')
 
         for fname in [self.all_reads1, self.all_reads2, self.references_fa]:
             if not os.path.exists(fname):
@@ -122,6 +121,28 @@ class Cluster:
             self.extern_progs = extern_progs
 
         self.random_seed = random_seed
+
+
+    def _set_up_input_files(self):
+        self.all_reads1 = os.path.join(self.root_dir, 'reads_1.fq')
+        self.all_reads2 = os.path.join(self.root_dir, 'reads_2.fq')
+        self.references_fa = os.path.join(self.root_dir, 'references.fa')
+
+        if os.path.exists(self.root_dir):
+            assert self.read_store is None
+            if not (os.path.exists(self.all_reads1) and os.path.exists(self.all_reads2)):
+                raise Error('Error making cluster. Reads files not found')
+            if not os.path.exists(self.references_fa):
+                raise Error('Error making cluster. References fasta not found')
+        else:
+            assert self.read_store is not None
+            assert self.reference_names is not None
+            try:
+                os.mkdir(self.root_dir)
+            except:
+                raise Error('Error making directory ' + seolf.root_dir)
+            self.read_store.get_reads(self.name, self.all_reads1, self.all_reads2)
+            self.refdata.write_seqs_to_fasta(self.references_fa, self.reference_names)
 
 
     def _clean_file(self, filename):
