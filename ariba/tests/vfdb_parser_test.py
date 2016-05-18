835e306c28887e19ac971dcf973bd376d4129c31
martinghunt <martin.g.hunt@gmail.com>
2016-05-18 10:31:51 +0000

    New class VfdbParser

---
+++ ariba/tests/vfdb_parser_test.py
@@ -0,0 +1,58 @@
+import unittest
+import filecmp
+import os
+from ariba import vfdb_parser
+
+modules_dir = os.path.dirname(os.path.abspath(vfdb_parser.__file__))
+data_dir = os.path.join(modules_dir, 'tests', 'data')
+
+
+class TestVfdbParser(unittest.TestCase):
+    def test_fa_header_to_name_pieces(self):
+        '''test _fa_header_to_name_pieces'''
+        tests = [
+            ('VF123(gi:1234) (abcD) foobar description [abc] [genus species]', ('VF123(gi:1234)', 'abcD', 'foobar description [abc]', 'genus species')),
+            ('F123(gi:1234) (abcD) foobar description [abc] [genus species]', None), # no V at start
+            ('VF123(gi:1234) (abcD) foobar description [abc]', None),  # missing genus species
+            ('VF123(gi:1234) abcD foobar description [abc] [genus species]', None), # brackets missing around abcD
+        ]
+
+        for header, expected in tests:
+            got = vfdb_parser.VfdbParser._fa_header_to_name_pieces(header)
+            self.assertEqual(expected, got)
+
+
+    def test_fa_header_to_name_and_metadata(self):
+        '''test _fa_header_to_name_and_metadata'''
+        headers = [
+            'VF123(gi:1234) (abcD) foobar description [abc] [genus species]',
+            'F123(gi:1234) (abcD) foobar description [abc] [genus species]', # no V at start
+            'VF123(gi:1234) (abcD) foobar description [abc]', # missing genus species
+            'VF123(gi:1234) abcD foobar description [abc] [genus species]', # brackets missing around abcD
+        ]
+
+        expected = [
+            ('abcD.VF123(gi:1234).genus_species', 'foobar description [abc]'),
+            (headers[1], None),
+            (headers[2], None),
+            (headers[3], None),
+        ]
+
+        assert len(headers) == len(expected)
+        for i in range(len(headers)):
+            got = vfdb_parser.VfdbParser._fa_header_to_name_and_metadata(headers[i])
+            self.assertEqual(expected[i], got)
+
+
+    def test_run(self):
+        '''test run'''
+        infile = os.path.join(data_dir, 'vfdb_parser_test_run.in.fa')
+        expected_tsv = os.path.join(data_dir, 'vfdb_parser_test_run.out.tsv')
+        expected_fa = os.path.join(data_dir, 'vfdb_parser_test_run.out.fa')
+        outprefix = 'tmp.vfdb_parser_test_run'
+        vp = vfdb_parser.VfdbParser(infile, outprefix)
+        vp.run()
+        self.assertTrue(filecmp.cmp(expected_tsv, outprefix + '.tsv', shallow=False))
+        self.assertTrue(filecmp.cmp(expected_fa, outprefix + '.fa', shallow=False))
+        os.unlink(outprefix + '.tsv')
+        os.unlink(outprefix + '.fa')
---
+++ ariba/vfdb_parser.py
@@ -0,0 +1,46 @@
+import re
+import pyfastaq
+
+class Error (Exception): pass
+
+name_regex = re.compile(r'^(?P<vfdb_id>V\S+\)) \((?P<name>\S+)\) (?P<description>.*\]) \[(?P<genus_etc>.*)\]$')
+
+class VfdbParser:
+    def __init__(self, infile, outprefix):
+        self.infile = infile
+        self.outprefix = outprefix
+
+
+    @classmethod
+    def _fa_header_to_name_pieces(cls, fa_header):
+        m = name_regex.search(fa_header)
+        if m is None:
+            return None
+        else:
+            return tuple([m.group(x) for x in ['vfdb_id', 'name', 'description', 'genus_etc']])
+
+
+    @staticmethod
+    def _fa_header_to_name_and_metadata(fa_header):
+        name_data = VfdbParser._fa_header_to_name_pieces(fa_header)
+        if name_data is None:
+            return fa_header, None
+        else:
+            vfdb_id, name, description, genus_etc = name_data
+            return name + '.' + vfdb_id + '.' + genus_etc.replace(' ', '_'), description
+
+
+    def run(self):
+        file_reader = pyfastaq.sequences.file_reader(self.infile)
+        fa_out = pyfastaq.utils.open_file_write(self.outprefix + '.fa')
+        tsv_out = pyfastaq.utils.open_file_write(self.outprefix + '.tsv')
+
+        for seq in file_reader:
+            seq.id, description = self._fa_header_to_name_and_metadata(seq.id)
+            if description is not None:
+                print(seq.id, '.', '.', description, sep='\t', file=tsv_out)
+            print(seq, file=fa_out)
+
+        pyfastaq.utils.close(fa_out)
+        pyfastaq.utils.close(tsv_out)
+
