ba7a3b8293353a103acc4374f600a57b858ee0e3
martinghunt <martin.g.hunt@gmail.com>
2016-05-24 07:58:42 +0000

    Code to load input files

--- ariba/aln_to_metadata.py
+++ ariba/aln_to_metadata.py
@@ -2,6 +2,7 @@ import os
 import sys
 import shutil
 import pyfastaq
+from ariba import sequence_variant
 
 class Error (Exception): pass
 
@@ -9,19 +10,41 @@ class AlnToMetadata:
     def __init__(self,
       aln_file,
       vars_file,
+      refs_are_coding
     ):
         self.padded_seqs = AlnToMetadata._load_aln_file(aln_file)
         self.variants = AlnToMetadata._load_vars_file(vars_file)
+        self.refs_are_coding = refs_are_coding
 
 
     @classmethod
     def _load_aln_file(cls, aln_file):
-        pass
+        seqs = {}
+        pyfastaq.tasks.file_to_dict(aln_file, seqs)
+        return seqs
 
 
     @classmethod
-    def _load_vars_file(cls, vars_file):
-        pass
+    def _load_vars_file(cls, vars_file, refs_are_coding):
+        var_type = 'p' if refs_are_coding else 'n'
+        f = pyfastaq.utils.open_file_read(vars_file)
+        variants = {}
+
+        for line in f:
+            try:
+                ref_name, variant, identifier, description = line.rstrip().split('\t')
+                variant = sequence_variant.Variant(var_type, variant, identifier)
+            except:
+                pyfastaq.utils.close(f)
+                raise Error('Error in this line of variants file:\n' + line)
+
+            if ref_name not in variants:
+                variants[ref_name] = []
+
+            variants[ref_name].append((variant, description))
+
+        pyfastaq.utils.close(f)
+        return variants
 
 
     def run(self, outprefix):
--- ariba/tests/aln_to_metadata_test.py
+++ ariba/tests/aln_to_metadata_test.py
@@ -3,7 +3,8 @@ import os
 import copy
 import shutil
 import filecmp
-from ariba import aln_to_metadata
+import pyfastaq
+from ariba import aln_to_metadata, sequence_variant
 
 modules_dir = os.path.dirname(os.path.abspath(aln_to_metadata.__file__))
 data_dir = os.path.join(modules_dir, 'tests', 'data')
@@ -12,9 +13,37 @@ data_dir = os.path.join(modules_dir, 'tests', 'data')
 class TestAlnToMetadata(unittest.TestCase):
     def test_load_aln_file(self):
         '''test _load_aln_file'''
-        pass
+        aln_file = os.path.join(data_dir, 'aln_to_metadata_load_aln_file.in.fa')
+        expected = {
+            'seq1': pyfastaq.sequences.Fasta('seq1', 'ABC-DE'),
+            'seq2': pyfastaq.sequences.Fasta('seq2', 'ABCQDE'),
+        }
+        got = aln_to_metadata.AlnToMetadata._load_aln_file(aln_file)
+        self.assertEqual(expected, got)
 
 
-    def test_load_vars_file(self):
-        '''test _load_vars_file'''
-        pass
+    def test_load_vars_file_good_file(self):
+        '''test _load_vars_file good input file'''
+        infile = os.path.join(data_dir, 'aln_to_metadata_load_vars_file_good.tsv')
+        variant1 = sequence_variant.Variant('p', 'A42B', 'id1')
+        variant2 = sequence_variant.Variant('p', 'C43D', 'id2')
+        variant3 = sequence_variant.Variant('p', 'E100F', 'id3')
+        expected = {
+            'seq1': [(variant1, 'description 1')],
+            'seq2': [(variant2, 'description 2'), (variant3, 'description 3')]
+        }
+        got = aln_to_metadata.AlnToMetadata._load_vars_file(infile, True)
+        self.assertEqual(expected, got)
+
+
+    def test_load_vars_bad_files(self):
+        '''test _load_vars_file bad input files'''
+        infiles = [
+            os.path.join(data_dir, 'aln_to_metadata_load_vars_file_bad.1.tsv'),
+            os.path.join(data_dir, 'aln_to_metadata_load_vars_file_bad.2.tsv')
+        ]
+
+        for infile in infiles:
+            with self.assertRaises(aln_to_metadata.Error):
+                aln_to_metadata.AlnToMetadata._load_vars_file(infile, True)
+
