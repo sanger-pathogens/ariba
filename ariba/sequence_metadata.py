48cbb6e3f889ccbb193e76fe75ab1082625abadf
martinghunt <martin.g.hunt@gmail.com>
2016-07-12 08:12:10 +0000

    New 6 column metadata format

--- ariba/sequence_metadata.py
+++ ariba/sequence_metadata.py
@@ -6,20 +6,26 @@ class Error (Exception): pass
 class SequenceMetadata:
     def __init__(self, line):
         try:
-            self.name, variant_type, variant_string, identifier, self.free_text = line.rstrip().split('\t')
+            self.name, self.seq_type, var_only, variant, variant_id, self.free_text = line.rstrip().split('\t')
         except:
             raise Error('Error parsing line of file:\n' + line)
 
-        self.variant_type = variant_type
+        if self.seq_type not in {'n', 'p'}:
+            raise Error('Error. Second column must be "n" or "p". Cannot continue. Line was:\n' + line)
 
-        if self.variant_type == '.':
+        if var_only not in {'0', '1'}:
+            raise Error('Error. Third column must be "0" or "1". Cannot continue. Line was:\n' + line)
+
+        self.variant_only = var_only == '1'
+
+        if variant == '.':
             self.variant = None
         else:
-            self.variant = sequence_variant.Variant(self.variant_type, variant_string, identifier)
+            self.variant = sequence_variant.Variant(self.seq_type, variant, variant_id)
 
 
     def __eq__(self, other):
-       return type(other) is type(self) and self.name == other.name and self.variant == other.variant and self.variant_type == other.variant_type and self.free_text == other.free_text
+       return type(other) is type(self) and self.name == other.name and self.seq_type == other.seq_type and self.var_only == other.var_only and self.variant == other.variant and self.free_text == other.free_text
 
 
     def __lt__(self, other):
@@ -27,7 +33,7 @@ class SequenceMetadata:
 
 
     def __hash__(self):
-        return hash((self.name, self.variant_type, str(self.variant), self.free_text))
+        return hash((self.name, self.seq_type, str(self.variant), self.free_text))
 
 
     def __str__(self):
@@ -37,7 +43,8 @@ class SequenceMetadata:
     def to_string(self, separator='\t'):
         return separator.join([
             self.name,
-            self.variant_type,
+            self.seq_type,
+            '1' if self.variant_only else '0',
             '.' if self.variant is None else str(self.variant),
             '.' if (self.variant is None or self.variant.identifier is None) else self.variant.identifier,
             self.free_text
