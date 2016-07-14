2af21f7b02a3525f802abbef743127b30127e641
andrewjpage <andrewjpage@gmail.com>
2016-07-14 13:52:51 +0100

    Revert "allow full VFDB to be downloaded"

--- ariba/ref_genes_getter.py
+++ ariba/ref_genes_getter.py
@@ -237,32 +237,26 @@ class RefGenesGetter:
         print('If you use this downloaded data, please cite:')
         print('"ARG-ANNOT, a new bioinformatic tool to discover antibiotic resistance genes in bacterial genomes",\nGupta et al 2014, PMID: 24145532\n')
 
-    def _get_from_vfdb_core(self, outprefix):
-        self._get_from_vfdb_common(self,'VFDB_setA_nt.fas.gz','core')
-		
-    def _get_from_vfdb_full(self, outprefix):
-        self._get_from_vfdb_common(self,'VFDB_setB_nt.fas.gz','full')
-	
-    def _get_from_vfdb_common(self,filename,info_text):
+    def _get_from_vfdb(self, outprefix):
         outprefix = os.path.abspath(outprefix)
         tmpdir = outprefix + '.tmp.download'
         current_dir = os.getcwd()
-        
+
         try:
             os.mkdir(tmpdir)
         except:
             raise Error('Error mkdir ' + tmpdir)
-        
-        zipfile = os.path.join(tmpdir, filename)
-        self._download_file('http://www.mgc.ac.cn/VFs/Down/'+filename, zipfile)
+
+        zipfile = os.path.join(tmpdir, 'VFDB_setA_nt.fas.gz')
+        self._download_file('http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz', zipfile)
         vparser = vfdb_parser.VfdbParser(zipfile, outprefix)
         vparser.run()
         shutil.rmtree(tmpdir)
         print('Extracted files.')
         final_fasta = outprefix + '.presence_absence.fa'
         final_tsv = outprefix + '.metadata.tsv'
-        
-        print('Extracted '+info_text+' DNA sequence dataset and metadata. Final files are:', final_fasta, final_tsv, sep='\n\t', end='\n\n')
+
+        print('Extracted core DNA sequence dataset and metadata. Final files are:', final_fasta, final_tsv, sep='\n\t', end='\n\n')
         print('You can use it with ARIBA like this:')
         print('ariba prepareref --ref_prefix', outprefix, 'output_directory\n')
         print('If you use this downloaded data, please cite:')
