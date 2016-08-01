caf5111cd84bf9f38c35f05b7f30c9fc9d2be655
martinghunt <martin.g.hunt@gmail.com>
2016-08-01 13:01:27 +0000

    Filter HET heterozygous SNP lines

--- ariba/report_filter.py
+++ ariba/report_filter.py
@@ -120,6 +120,7 @@ class ReportFilter:
         return ReportFilter._flag_passes_filter(report_dict['flag'], self.exclude_flags) \
                    and report_dict['pc_ident'] >= self.min_pc_ident \
                    and report_dict['ref_base_assembled'] >= self.min_ref_base_assembled \
+                   and report_dict['var_type'] != 'HET'
 
 
     def _filter_list_of_dicts(self, dicts_list):
