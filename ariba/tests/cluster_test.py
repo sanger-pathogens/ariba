7391e24c28ca34a24778725362e1533b7199dc71
martinghunt <martin.g.hunt@gmail.com>
2016-08-01 12:25:30 +0000

    add test test_full_run_ok_samtools_snp_var_only_noncoding

--- ariba/tests/cluster_test.py
+++ ariba/tests/cluster_test.py
@@ -370,6 +370,22 @@ class TestCluster(unittest.TestCase):
         shutil.rmtree(tmpdir)
 
 
+    def test_full_run_ok_samtools_snp_var_only_noncoding(self):
+        '''test complete run where samtools calls a snp in a presence/absence noncoding sequence'''
+        fasta_in = os.path.join(data_dir, 'cluster_test_full_run_ok_samtools_snp_var_only_noncoding.fa')
+        tsv_in = os.path.join(data_dir, 'cluster_test_full_run_ok_samtools_snp_var_only_noncoding.metadata.tsv')
+        refdata = reference_data.ReferenceData([fasta_in], [tsv_in])
+        tmpdir = 'tmp.cluster_test_full_run_ok_samtools_snp_var_only_noncoding'
+        shutil.copytree(os.path.join(data_dir, 'cluster_test_full_run_ok_samtools_snp_var_only_noncoding'), tmpdir)
+        c = cluster.Cluster(tmpdir, 'cluster_name', refdata, spades_other_options='--only-assembler', total_reads=148, total_reads_bases=13320)
+        c.run()
+        expected = [
+            'ref_seq\t0\t1\t147\t148\tcluster_name\t96\t96\t100.0\tcluster_name.scaffold.1\t335\t39.8\t0\tHET\t.\t.\t.\t.\t.\t.\t.\t.\t137\t137\tG\t63\tA\t32,31\t.\tGeneric description of ref_seq'
+        ]
+        self.assertEqual(expected, c.report_lines)
+        shutil.rmtree(tmpdir)
+
+
     def test_full_run_ok_samtools_snp_known_position_pres_abs_noncoding(self):
         '''test complete run where samtools calls a snp at a known snp location in a presence/absence noncoding'''
         fasta_in = os.path.join(data_dir, 'cluster_test_full_run_ok_samtools_snp_known_position_pres_abs_noncoding.fa')
