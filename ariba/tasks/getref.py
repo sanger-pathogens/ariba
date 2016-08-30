18ed4c8c875f87d50116262ce6154ba1a9d7126b
martinghunt <martin.g.hunt@gmail.com>
2016-08-30 13:03:33 +0000

    Remove genetic code option from getref

--- ariba/tasks/getref.py
+++ ariba/tasks/getref.py
@@ -5,7 +5,6 @@ from ariba import ref_genes_getter
 def run(options):
     getter = ref_genes_getter.RefGenesGetter(
         options.db,
-        genetic_code=options.genetic_code,
         version=options.version
     )
     getter.run(options.outprefix)
