f0a50e9716d5cecb87a2c0d07cfc8762b08fddcd
martinghunt <martin.g.hunt@gmail.com>
2017-02-21 13:08:43 +0000

    Add method _make_plot, fix some file format issues

--- ariba/mic_plotter.py
+++ ariba/mic_plotter.py
@@ -8,9 +8,24 @@ class Error (Exception): pass
 regex_string_to_float = re.compile(r'\s*(?P<lt_or_gt>[<>]?)\s*(?P<equals>=?)\s*(?P<number>[0-9.]+)\s*$')
 
 class MicPlotter:
-    def __init__(self, mic_file, summary_file):
+    def __init__(self,
+      antibiotic,
+      mic_file,
+      summary_file,
+      outprefix,
+      main_title=None,
+      plot_height=15,
+      plot_width=15,
+      log_y=True,
+    ):
+        self.antibiotic = antibiotic
         self.mic_file = mic_file
         self.summary_file = summary_file
+        self.outprefix = outprefix
+        self.main_title = self.antibiotic if main_title is None else main_title
+        self.plot_height = plot_height
+        self.plot_width = plot_width
+        self.log_y = log_y
 
 
     @classmethod
@@ -57,7 +72,8 @@ class MicPlotter:
         data = {}
 
         with open(infile) as f:
-            reader = csv.DictReader(f, delimiter='\t')
+            reader = csv.DictReader(f, delimiter=',')
+
             if reader.fieldnames[0] != 'name':
                 raise Error('Error. Expected first column of summary file "' + infile + '" to be "name"')
 
@@ -153,7 +169,7 @@ class MicPlotter:
             output_columns[combination] = [(1 if x in combination else 0) for x in all_mutations]
 
         with open(outfile, 'w') as f:
-            print('Mutation\t', end='', file=f)
+            print('Mutation', end='', file=f)
             for x in combinations:
                 print('\t', '.'.join(x), sep='', end='', file=f)
             print('', file=f)
@@ -162,3 +178,114 @@ class MicPlotter:
                 row = [all_mutations[i]] + [output_columns[x][i] for x in combinations]
                 print(*row, sep='\t', file=f)
 
+
+    def _make_plot(self,
+      samples_file,
+      dots_file,
+    ):
+        r_script = self.outprefix + '.R'
+
+        try:
+            f = open(r_script, 'w')
+        except:
+            raise Error('Error opening R script for writing "' + r_script + '"')
+
+        libraries = ['ggplot2', 'RColorBrewer', 'reshape2', 'cowplot', 'latex2exp']
+        for lib in libraries:
+            print('library(', lib, ')', sep='', file=f)
+
+        print('samples = read.csv(file="', samples_file, r'''", header=TRUE, sep="\t")''', sep='', file=f)
+        print('dots = read.csv(file="', dots_file, r'''", header=TRUE, sep="\t")''', sep='', file=f)
+
+        if self.log_y:
+            print('use.log = TRUE', file=f)
+        else:
+            print('use.log = FALSE', file=f)
+
+        print(r'''
+dots.melt = melt(dots)
+colnames(dots.melt) <- c("var1", "var2", "value")
+
+accent <- brewer.pal(8, 'Accent')
+        accentPalette <- colorRampPalette(accent)
+        ncols <- length(as.vector(unique(samples$Mutations)))
+        cols <- accentPalette(ncols)
+
+
+names(cols) <- sort(as.vector(unique(samples$Mutations)))
+setcols <- c()
+
+for (i in 1:nrow(dots.melt)){
+    if (dots.melt[i,3]==1){
+        setcols <- c(setcols, cols[as.vector(dots.melt[i,2])])
+    }
+    else{
+      setcols <- c(setcols, NA)
+    }
+}
+dots.melt <- cbind(dots.melt, setcols)
+
+mutations <- levels(dots.melt$var1)
+i <- match("without_mutation", mutations)
+if (!is.na(i)) {
+    mutations <- c(mutations[-i], "without_mutation")
+}
+
+
+dotplot <- ggplot(dots.melt, aes(x=var2, y=var1)) +
+  geom_point(aes(fill=setcols, colour=setcols), size=8) +
+          scale_fill_identity()+
+          scale_colour_identity()+
+          ylim(rev(mutations)) +
+  theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_text(size=18),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position="none")
+
+range.mics <- c(0,0.001,0.0025,0.0075,0.015,0.03,0.06,0.125,0.25,0.5,1,2,4,8,16,32,64,128,256,512,1024)
+if (use.log){ final.mics <- log(range.mics) }else{ final.mics <- range.mics }
+
+violinplot <- ggplot(data=samples, aes(x=Mutations, y=log(MIC))) +
+    geom_point(aes(color=Mutations), position = position_jitter(width=0.01, height=0.01), size=4, alpha=.5) +
+    geom_violin(aes(color=Mutations),alpha=.10, show.legend = FALSE) +
+    ylab(expression(paste("MIC ", mu, "g/mL"))) +
+    scale_colour_manual(values = cols) +
+    ggtitle("''' + self.main_title + r'''") +
+    scale_y_continuous(breaks=final.mics, labels=range.mics) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.border = element_blank(),
+            axis.line.x = element_line(color="black"),
+            axis.line.y = element_line(color="black"),
+            axis.title.x = element_blank(),
+            axis.title.y = element_text(size=22),
+            axis.text.x = element_blank(),
+            axis.text.y = element_text(size=24),
+            axis.title = element_text(size=20),
+            plot.title = element_text(lineheight=.6, size = 24, hjust=.5, face="bold"),
+            legend.position="none")
+
+plot_grid(violinplot, dotplot, ncol=1, align="v", rel_heights=c(3,1))
+''', file=f)
+
+        print('ggsave("', self.outprefix, '.pdf", height=', self.plot_height, ', width=', self.plot_width, ')', sep='', file=f)
+        f.close()
+        common.syscall('R CMD BATCH ' + r_script)
+
+
+    def run(self):
+        mic_data = MicPlotter._load_mic_file(self.mic_file)
+        summary_data = MicPlotter._load_summary_file(self.summary_file)
+        boxplot_tsv = self.outprefix + '.boxplot.tsv'
+        all_mutations, combinations = MicPlotter._to_boxplot_tsv(summary_data, mic_data, self.antibiotic, boxplot_tsv)
+        dots_tsv = self.outprefix + '.dots.tsv'
+        MicPlotter._to_dots_tsv(all_mutations, combinations, dots_tsv)
+        self._make_plot(boxplot_tsv, dots_tsv)
+        
