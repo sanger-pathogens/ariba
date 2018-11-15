4f635c70c2f6a0d4cb1f00230fe5f2424d0dc998
Martin Hunt <martin.g.hunt@gmail.com>
2018-11-15 14:59:30 +0000

    New function load_mutations

--- ariba/tb.py
+++ ariba/tb.py
@@ -1,9 +1,15 @@
 import csv
 import json
+import os
+import re
+import sys
 
 from Bio import SeqIO
 
 from ariba import flag
+
+data_dir = os.path.join(os.path.dirname(__file__), 'tb_data')
+assert os.path.exists(data_dir)
 
 
 def report_to_resistance_dict(infile):
@@ -62,3 +68,69 @@ def genbank_to_gene_coords(infile, genes):
                     coords[gene_name] = {'end': int(feature.location.start), 'start': int(feature.location.end) - 1}
 
     return coords
+
+
+def load_mutations(gene_coords, mutation_to_drug_json, variants_txt, upstream_before=100):
+    '''Load mutations from "mykrobe-style" files. mutation_to_drug_json is json file
+    of mutation -> list of drugs. variants_txt is text file of variants used my mykrobe's
+    make probes. gene_coords should be dict of gene coords made by the function
+    genbank_to_gene_coords'''
+    with open(mutation_to_drug_json) as f:
+        drug_data = json.load(f)
+
+    mutations = []
+    genes_with_indels = set()
+    genes_need_upstream = set()
+    genes_non_upstream = set()
+
+    with open(variants_txt) as f:
+        for line in f:
+            gene, variant, d_or_p = line.rstrip().split('\t')
+            coding = 0 if gene == 'rrs' else 1
+            d = {'gene': gene, 'var': variant, 'coding': coding, 'upstream': False}
+            drug_data_key = d['gene'] + '_' + d['var']
+            if drug_data_key not in drug_data:
+                print('KEY', drug_data_key, 'NOT FOUND', file=sys.stderr)
+            else:
+                d['drugs'] = ','.join(sorted(drug_data[drug_data_key]))
+
+            if d_or_p == 'DNA' and gene != 'rrs':
+                assert gene != 'rrs'
+                re_match = re.match('([ACGT]+)(-?[0-9]+)([ACGTX]+)', d['var'])
+                try:
+                    ref, pos, alt = re_match.groups()
+                except:
+                    print('regex error:', d['var'], file=sys.stderr)
+                    continue
+
+                pos = int(pos)
+                if len(ref) != len(alt):
+                    genes_with_indels.add(d['gene'])
+                    continue
+                elif pos > 0:
+                    print('ignoring:', d, file=sys.stderr)
+                    continue
+                elif pos < 0:
+                    this_gene_coords = gene_coords[d['gene']]
+                    d['upstream'] = True
+                    if this_gene_coords['start'] < this_gene_coords['end']:
+                        variant_pos_in_output_seq = upstream_before + pos + 1
+                    else:
+                        variant_pos_in_output_seq = upstream_before + pos + 1
+                    assert variant_pos_in_output_seq > 0
+                    d['var'] = ref + str(variant_pos_in_output_seq) + alt
+                    d['original_mutation'] = variant
+                    genes_need_upstream.add(d['gene'])
+                elif pos == 0:
+                    print('Zero coord!', d, file=sys.stderr)
+                    continue
+                else:
+                    print('deal with?', d, file=sys.stderr)
+                    continue
+
+            mutations.append(d)
+            if not d['upstream']:
+                genes_non_upstream.add(d['gene'])
+
+    return mutations, genes_with_indels, genes_need_upstream, genes_non_upstream
+
--- ariba/tests/tb_test.py
+++ ariba/tests/tb_test.py
@@ -1,5 +1,7 @@
 import unittest
+import json
 import os
+
 from ariba import tb
 
 modules_dir = os.path.dirname(os.path.abspath(tb.__file__))
@@ -31,3 +33,48 @@ class TestTb(unittest.TestCase):
         got = tb.genbank_to_gene_coords(infile, genes)
         self.assertEqual(expect, got)
 
+
+
+    def test_load_mutations(self):
+        '''test load_mutations'''
+        mutation_to_drug_json = 'tmp.tb.load_mutations.json'
+        variants_txt = 'tmp.tb.load_mutations.txt'
+
+        gene_coords = {
+            'gene1': {'start': 42, 'end': 45},
+            'gene2': {'start': 100, 'end': 50},
+            'gene3': {'start': 200, 'end': 300},
+        }
+
+        mutation_to_drug = {
+            'gene1_A-5T': ['drug1'],
+            'gene1_A5C': ['drug1', 'drug2'],
+            'gene2_A-3G': ['drug4'],
+            'gene2_A10C': ['drug5'],
+            'gene3_AC10A': ['drug5'],
+        }
+        with open(mutation_to_drug_json, 'w') as f:
+            json.dump(mutation_to_drug, f, indent=4, sort_keys=True)
+
+        with open(variants_txt, 'w') as f:
+            print('gene1', 'A-5T', 'DNA', sep='\t', file=f)
+            print('gene1', 'A5C', 'PROT', sep='\t', file=f)
+            print('gene2', 'A-3G', 'DNA', sep='\t', file=f)
+            print('gene2', 'A10C', 'PROT', sep='\t', file=f)
+            print('gene3', 'AC10A', 'DNA', sep='\t', file=f)
+        got_mutations, got_genes_with_indels, got_genes_need_upstream, got_genes_non_upstream = tb.load_mutations(gene_coords, mutation_to_drug_json, variants_txt, upstream_before=10)
+        expect_mutations = [
+            {'gene': 'gene1', 'var': 'A6T', 'coding': 1, 'upstream': True, 'drugs': 'drug1', 'original_mutation': 'A-5T'},
+            {'gene': 'gene1', 'var': 'A5C', 'coding': 1, 'upstream': False, 'drugs': 'drug1,drug2'},
+            {'gene': 'gene2', 'var': 'A8G', 'coding': 1, 'upstream': True, 'drugs': 'drug4', 'original_mutation': 'A-3G'},
+            {'gene': 'gene2', 'var': 'A10C', 'coding': 1, 'upstream': False, 'drugs': 'drug5'}
+        ]
+        expect_genes_with_indels = {'gene3'}
+        expect_genes_need_upstream = {'gene1', 'gene2'}
+        expect_genes_non_upstream = {'gene1', 'gene2'}
+        self.assertEqual(expect_mutations, got_mutations)
+        self.assertEqual(expect_genes_with_indels, got_genes_with_indels)
+        self.assertEqual(expect_genes_need_upstream, got_genes_need_upstream)
+        self.assertEqual(expect_genes_non_upstream, got_genes_non_upstream)
+        os.unlink(mutation_to_drug_json)
+        os.unlink(variants_txt)
